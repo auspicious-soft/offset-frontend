9ab9cdd5e90d0f1db179155d09c6f316a210a927
Jaser Sokoli <j.sokoli@elogic.com.ua>
2025-04-21 16:22:26 +0530

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ const HomePage = () => {
 
       <div className={`${jost.className} flex flex-col lg:flex-row min-h-screen`}>
         <div className=" w-full lg:w-[45%]  2xl:w-[40%] h-auto  lg:mn-h-full relative object-contain">
-          {/* Background Image */}
+          {/* Background Image */}   
           <Image
             src="/images/loginBG.png"
             alt="Login Image"
